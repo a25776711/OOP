6fd508261d952a342dd6f8f8c6b08a4cce0e28a5
kaven <71436246+a25776711@users.noreply.github.com>
2025-03-10 20:53:24 +0800

    一般殭屍檔案建立測試完成

--- files.cmake
+++ files.cmake
@@ -1,5 +1,7 @@
 set(SRC_FILES
     App.cpp
+        Zombi/Zombi.hpp
+        Zombi/BucketZombies.hpp
 )
 
 set(INCLUDE_FILES
---
+++ include/Zombi/Zombi.hpp
@@ -0,0 +1,16 @@
+//
+// Created by a2577 on 25-3-10.
+//
+
+#ifndef ZOMBI_HPP
+#define ZOMBI_HPP
+class Zombi {
+    public:
+        Zombi();
+        ~Zombi();
+    private:
+        int x,y;
+        int HP;
+        int ATK;
+};
+#endif //ZOMBI_HPP
