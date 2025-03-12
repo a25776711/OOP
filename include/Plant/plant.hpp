b00d23a520221a2e90badf309b935755e62a8d78
kaven <71436246+a25776711@users.noreply.github.com>
2025-03-12 23:53:51 +0800

    Update plant.hpp

--- include/Plant/plant.hpp
+++ include/Plant/plant.hpp
@@ -4,5 +4,20 @@
 
 #ifndef PLANT_HPP
 #define PLANT_HPP
+#include "Util/GameObject.hpp"
 
+class Plant: public Util::GameObject{
+    public:
+        int hp;
+        int maxhp;
+        Plant(hp,const const std::vector<std::string>& Path,int interval):GameObject() {
+                m_Drawable = std::make_shared<Util::Animation>(AnimationPaths, false, interval, false, 0);
+        }
+        ~Plant();
+    private:
+        std::string m_ImagePath;
+        //位置
+        int x,y;
+
+};
 #endif //PLANT_HPP
