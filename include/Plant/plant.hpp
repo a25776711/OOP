8c9ce227835cc99a98e6a356fbeef185f130b960
kaven <71436246+a25776711@users.noreply.github.com>
2025-03-14 14:09:53 +0800

    Update plant.hpp

--- include/Plant/plant.hpp
+++ include/Plant/plant.hpp
@@ -10,16 +10,17 @@
 class Plant: public Util::GameObject{
     public:
 
-        Plant(hp,const const std::vector<std::string>& Path,int interval):GameObject() {
-                m_Drawable = std::make_shared<Util::Animation>(Path, false, interval, false, 0);
-        }
-        ~Plant();
-    private:
-        std::string m_ImagePath;
-        //位置
-        int x,y;
-        int hp;
-        int maxhp;
+    Plant(const const std::vector<std::string>& Path,int interval):GameObject() {
+        m_Drawable = std::make_shared<Util::Animation>(Path, false, interval, false, 0);
+    }
+    ~Plant();
+    [[nodiscard]] bool GetVisibility() const { return m_Visible;}
+    [[nodiscard]]const std::string& GetImagePath() const { return m_ImagePath; }
+    [[nodiscard]] std::vector<int> GetPosition() const { return {x,y}; }
 
+    private:
+    std::string m_ImagePath;
+    //位置
+    int x,y;
 };
 #endif //PLANT_HPP
