f98cdf038ea03c9330b7248b7bc27034b4a2bce9
kaven <71436246+a25776711@users.noreply.github.com>
2025-04-29 19:42:15 +0800

    殭屍攻擊修正

--- src/Zombi/zombi.cpp
+++ src/Zombi/zombi.cpp
@@ -7,7 +7,7 @@
 void zombi::HitCheck(std::shared_ptr<Plant> plant) {
     auto pos = plant->GetPosition();
     if((m_Transform.translation.x - pos.x <= 30) && 
-       (abs(m_Transform.translation.y - pos.y) <= 30) &&
+       (abs(m_Transform.translation.y - pos.y) <= 40) &&
        (m_state != zombistate::eat && m_state != zombistate::coldeat)) {
         StartEat();
     }
