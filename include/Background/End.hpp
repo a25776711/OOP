66f6873facbefdff6fd31970a63b9ee0eed3abb7
Hannho <bighanhe897@gmail.com>
2025-06-06 13:45:42 +0800

    修

--- include/Background/End.hpp
+++ include/Background/End.hpp
@@ -11,8 +11,10 @@ class end : public GameObject {
     public:
         end():GameObject(std::make_unique<Util::Image>(RESOURCE_DIR"/Background/losegame.png"), -10) {
             m_Transform.scale = glm::vec2(0.1f, 0.1f);
+            SetZIndex(100);
         }
         void Big() {
+            if (m_Transform.scale == glm::vec2(1.0f, 1.0f)) {return;}
             m_Transform.scale = glm::vec2(m_Transform.scale.x+ 0.1f, m_Transform.scale.y+0.1f);
         }
 };
